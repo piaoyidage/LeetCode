6361a532ba3d44c5e6e0bef9468c781b81f3ab78
piaoyidage <longyixiaoge@gmail.com>
2018-08-28 19:28:11 +0800

    两数相加

---
+++ js/2/README.md
@@ -0,0 +1,44 @@
+# 2. 两数相加
+
+## 题目
+
+给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+
+你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+## 解答
+
+第一时间可能会写下类似如下的代码：
+
+```js
+const addTwoNumbers = function(l1, l2) {
+	const sum = getNum(l1) + getNum(l2)
+    return getNumList(sum)
+}
+
+// 获取链表表示的数字
+const getNum = function(l) {
+	const numbers = [l.val]
+	let nextNode = l.next
+	while (nextNode) {
+		numbers.push(nextNode.val)
+		nextNode = nextNode.next
+	}
+	return parseInt(numbers.reverse().join('')
+}
+
+// 构造数字链表
+const getNumList = function(num) {
+	const numArr = (num + '').split('').reverse().map(item => parseInt(item))
+	return numArr
+}；
+```
+
+但是上面代码没有考虑数字相加会溢出，正确的方法是：对每一位数字相加减，然后拼接。
+
---
+++ js/2/source.js
@@ -0,0 +1,67 @@
+/*
+* @Author: maoying.hu
+* @Date:   2018-08-28 16:53:13
+* @Last Modified by:   maoying.hu
+* @Last Modified time: 2018-08-28 19:23:28
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+	let numArr1 = getNum(l1)
+	let numArr2 = getNum(l2)
+
+	const len1 = numArr1.length
+    const len2 = numArr2.length
+
+    // 将两个数组处理成一样的长度，用 0 填充
+    if (len1 < len2) {
+        numArr1 = numArr1.concat(Array(len2 - len1).fill(0))
+    } else {
+        numArr2 = numArr2.concat(Array(len1 - len2).fill(0))
+    }
+
+    const max = len1 > len2 ? len1 : len2
+    const sum = []
+    // 进位
+    let carry = 0
+    let cur = 0
+    let i = 0
+    while (i < max) {
+        cur = parseInt(numArr1[i]) + parseInt(numArr2[i]) + carry
+        if (cur >= 10) {
+            carry = 1
+            sum.push(cur - 10)
+        } else {
+            carry = 0
+            sum.push(cur)
+        }
+        i++
+    }
+    if (carry === 1) {
+        sum.push(1)
+    }
+
+    return sum
+}
+
+// 获取链表表示的数字
+const getNum = function(l) {
+	const numbers = [l.val]
+	let nextNode = l.next
+	while (nextNode) {
+		numbers.push(nextNode.val)
+		nextNode = nextNode.next
+	}
+	return numbers.join('')
+}
